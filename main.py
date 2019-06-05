24f80a88ed1a2389f6ee9d775cc725215b8d7626
AradAshrafi <Arad.Ashrafi@gmail.com>
2019-06-05 14:27:14 +0430

    Separate Test,Train and their Input Output from basic data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from utils.file_operation import FileOperator
 
+train_ratio = 0.8
+
 if __name__ == '__main__':
     file_operator = FileOperator()
-    csv_file = file_operator.read_from_file()
-    print(csv_file)
+    csv_file = file_operator
+    train_data_input, train_data_output, test_data_input, test_data_output = file_operator.get_test_train_data(
+        train_ratio=train_ratio)
--- utils/file_operation.py
+++ utils/file_operation.py
@@ -6,6 +6,25 @@ class FileOperator:
     def __init__(self, path=TWO_CLASSES_PATH):
         self.path = path
 
-    def read_from_file(self):
+    # read from specified CSV file
+    def __read_from_file(self):
         df = pandas.read_csv(self.path)
-        return df
+        return df.values
+
+    # determine test and train data from read CSV file
+    def get_test_train_data(self, train_ratio):
+        data = self.__read_from_file()
+        data_len = data.__len__()
+        X_train, Y_train = separate_input_output(data[:int(data_len * train_ratio)])
+        X_test, Y_test = separate_input_output(data[int(train_ratio * data_len):])
+        return X_train, Y_train, X_test, Y_test
+
+
+# consider last column of input as Label and separate X and Y with that assumption
+def separate_input_output(labeled_data):
+    X = []
+    Y = []
+    for current_data in labeled_data:
+        X.append([current_data[:len(current_data) - 1]])
+        Y.append(current_data[len(current_data) - 1])
+    return X, Y
